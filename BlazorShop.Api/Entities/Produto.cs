a46e9a20fd5368da9ba6ec1bf8767aeaf8859e6e
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-11-27 17:38:40 -0300

    Migrações e Entidades

---
+++ BlazorShop.Api/Entities/Produto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorShop.Api.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [MaxLength(80)]
+        public string Nome { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string Descricao { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string ImagemUrl { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+
+        public ICollection<CarrinhoItem> Itens { get; set; } = new List<CarrinhoItem>();
+    }
+}
