c5c7364c9d16d6030e7fad9c5f975f35b809a427
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-11-27 21:10:22 -0300

    Web Reboot

--- BlazorShop.Web/Program.cs
+++ BlazorShop.Web/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using BlazorShop.Web;
 using BlazorShop.Web.Services;
 using Microsoft.AspNetCore.Components.Web;
@@ -7,11 +8,20 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var BaseUrl = "https://localhost:7110";
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(BaseUrl) });
-
-
+var baseUrl = "https://localhost:7150";
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(baseUrl)
+});
 builder.Services.AddScoped<IProdutoService, ProdutoService>();
+builder.Services.AddScoped<ICarrinhoCompraService, CarrinhoCompraService>();
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<IGerenciaProdutosLocalStorageService,
+    GerenciaProdutosLocalStorageService>();
+
+builder.Services.AddScoped<IGerenciaCarrinhoItensLocalStorageService,
+    GerenciaCarrinhoItensLocalStorageService>();
 
 await builder.Build().RunAsync();
